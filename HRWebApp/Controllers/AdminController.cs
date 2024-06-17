6817c027cfd28d0217506269bf70d6e6e1180feb
unknown <longthanh.dev@gmail.com>
2024-06-17 12:23:59 +0700

    final 2

---
+++ HRWebApp/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using HRWebApp.Services;
+
+namespace HRWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly EmployeeService _employeeService;
+
+        public AdminController()
+        {
+            _employeeService = new EmployeeService();
+        }
+
+        // GET: Admin
+        public async Task<ActionResult> Index()
+        {
+            var employees = await _employeeService.GetEmployeesAsync();
+            int totalEmployees = employees.Count();
+            ViewBag.TotalEmployees = totalEmployees;
+            return View();
+        }
+    }
+}
---
+++ HRWebApp/Services/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json.Linq;
+
+namespace HRWebApp.Services
+{
+    public class EmployeeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:4000/api/");
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+
+            var response = await _httpClient.GetAsync("employee");
+            response.EnsureSuccessStatusCode();
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var parsedJson = JObject.Parse(responseJson);
+            var employees = parsedJson["data"].ToObject<List<Employee>>();
+
+            return employees;
+        }
+    }
+
+    public class Employee
+    {
+        public string _id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int vacationDays { get; set; }
+        public int paidToDate { get; set; }
+        public int paidLastYear { get; set; }
+        public double payRate { get; set; }
+        public int payRateId { get; set; }
+    }
+}
